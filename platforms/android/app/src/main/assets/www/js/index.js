0ffc6c736c65c426caad21ea69ea7d8c785b6126
MinGra <mingrachina@gmail.com>
2018-02-12 00:36:02 +0800

    unfinished but I would like to commit daily. commited 20180211

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,21 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//SPA中用到的常量
+const TEXT_CONTENT_STYLE_ROWS = "5";
+
+//SPA中用到的全局变量
+var isEditing = false;
+var noteArray = new Array();
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -23,24 +12,55 @@ var app = {
     },
 
     // deviceready Event Handler
-    //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+        //native相关事件绑定
+        document.addEventListener("backbutton", onBackKeyDown, false);
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        //native无关事件绑定
+        document.getElementById("note-save-button").addEventListener("click",saveNote,true);
+        
+        //前端布局相关
+        document.getElementById("note-content").rows=TEXT_CONTENT_STYLE_ROWS;
+  
+        updateNotesView();
     }
 };
 
 app.initialize();
+
+//应用中绑定的事件处理函数
+function onBackKeyDown() {
+    if(isEditing) {
+        location.reload();
+        isEditing = false;
+    } else {
+        navigator.app.exitApp();
+    }
+ }
+
+function saveNote(){
+    // alert("saveing");
+    var note = {};
+
+    note.title = document.getElementById('note-title').value;
+    note.content = document.getElementById('note-content').value;
+    note.time = new Date();
+    noteArray.unshift(note);
+    window.localStorage.setItem('noteArray',JSON.stringify(noteArray));
+    updateNotesView();
+}
+
+//其他函数
+//显示window.localStorage中存储的备忘录的信息
+function updateNotesView() {
+    //从window.localStorage中获取存储的备忘录的信息
+    if(JSON.parse(window.localStorage.getItem('noteArray'))!=null) {
+        noteArray = JSON.parse(window.localStorage.getItem('noteArray'));
+    }
+    //显示在前端
+    console.log(noteArray);
+}
+
+
